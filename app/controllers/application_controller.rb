fd535ff0f05feef0247dd67f381089c74b002481
DO72 <michaellaw@gmx.com>
2022-05-10 23:00:12 -0400

    cheating on html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,34 +15,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/weapons" do
-    url = 'https://finalfantasy.fandom.com/wiki/Final_Fantasy_Tactics_weapons'
-    html = Nokogiri::HTML(URI.open(url))
-    weapons_list = [] 
-    # full-width FFT article-table
-    html.css('table[class="full-width FFT article-table"]').each do |table|
-      # name = nil
-      # desc = nil
-      # atk  = nil
-      weapon = {}
-      table.search('tr').search('td').each_with_index { |el, i|
-        p el.content
-        case i
-        when 0
-          weapon[:atk] = el.content
-        when 1
-          weapon[:mag] = el.content
-        when 2
-          weapon[:def] = el.content
-        else
-          weapon[:desc] = el.content
-        end
-      }
-      table.search('tr').search('th > a > span').each do |element|
-        # weapon[:name] = element.content[0].content
-        # weapons_list << element.children[0].content
-      end
-      weapons_list << weapon
-    end
+    url = 'https://gamefaqs.gamespot.com/psp/937312-final-fantasy-tactics-the-war-of-the-lions/faqs/76070/weapons'
+    weapons_list = []
+    html = Nokogiri::HTML5(URI.open(url))
+    html.search('table').each {|t| p t.class}
     {weapson: weapons_list}.to_json
   end
 
