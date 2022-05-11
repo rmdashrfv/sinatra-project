7e4a42108a0520af02fe8ebe024915e4c5bdf554
DO72 <michaellaw@gmx.com>
2022-05-10 23:36:19 -0400

    Weapons API with categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,27 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/weapons" do
-    url = 'https://gamefaqs.gamespot.com/psp/937312-final-fantasy-tactics-the-war-of-the-lions/faqs/76070/weapons'
+    url = 'http://www.ffmages.com/final-fantasy-tactics/weapons/'
     weapons_list = []
     html = Nokogiri::HTML5(URI.open(url))
-    html.search('table').each {|t| p t.class}
-    {weapson: weapons_list}.to_json
+    weapon_categories = []
+    html.search('h2').each do |h2|
+      weapon_categories << h2.content
+    end
+    html.search('table').each_with_index { |table, table_index|
+      category = weapon_categories[table_index]
+      table.css('tbody > tr').each_with_index { |tr, tr_index|
+        next if tr_index == 0
+        weapon = {img: nil, name: nil, atk: nil, evade: nil, location: nil, cost: nil, attributes: nil, category: category}
+        tr.css('td').each_with_index { |td, td_index|
+          next if td_index == 0
+          p td.content
+          weapon[weapon.keys[td_index]] = td.content
+        }
+        weapons_list << weapon
+      }
+    }
+    {weapons: weapons_list}.to_json
   end
 
 end
